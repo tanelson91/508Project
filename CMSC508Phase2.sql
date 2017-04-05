314132c7891bf1d9e53a0b4eec0c92367e934348
Brandon Harris <harrisbm3@users.noreply.github.com>
2017-04-05 14:42:25 -0400

    Updated Order
    Updated the order of which the tables are created, should be no issue running this now (I think)
    Added the Address table, Somehow we over looked putting this table in the Model as well.

--- CMSC508Phase2.sql
+++ CMSC508Phase2.sql
@@ -5,6 +5,32 @@ Last_Name varchar2(25),
 Wage number (7),
 primary key (EmpID));
 
+create table Shipment(
+ProNumber number(8) not null,
+DueDate date,
+TotalWeight number(8),
+Price number(8),
+Primary key (ProNumber));
+
+create table ShipmentCondition(
+ProNumber not null,
+Damaged number(4),
+Short number(4),
+over number(4),
+Primary key(ProNumber));
+
+create table Entity(
+EntityID number(10) not null,
+Type varchar2(25),
+Primary key(EntityID));
+
+create table Address(
+AddressID number(10) not null,
+StreetAddress varchar(40),
+City varchar(20),
+ZipCode number(5)
+Primary key(AddressID));
+
 create table LoadEvent(
 EventID number (10) not null,
 "Date" DATE,
@@ -12,21 +38,30 @@ ProNumber,
 Primary key (EventID),
 Foreign key (ProNumber) references Shipments(ProNumber));
 
-create table Trailer(
-TrailerNumber not null,
-DoorNumber,
-TerminalID,
-Primary key (TrailerNumber),
-Foreign key (TrailerNumber) references Entity(EntityID),
-Foreign key (DoorNumber) references Door(DoorNumber),
-Foreign key (TerminalID) references Terminal(TerminalID));
+create table Terminal(
+TerminalID not null,
+StreetAddress,
+ZipCode,
+City,
+State,
+Primary key(TerminalID),
+Foreign key(TerminalID) references Entity(Ent),
+Foreign key(StreetAddress) references Address(StreetAddress),
+Foreign key(ZipCode) references Address(ZipCode),
+Foreign key(City) references Address(City));
 
-create table CityHaulRoute(
-TrailerNumber not null,
-Route,
-Primary key(Trailernumber),
-Foreign key(Trailernumber) references Trailer(TrailerNumber),
-Foreign key(Route) references Entity(EntityID));
+create table Customer(
+ID number(7) not null,
+Name varchar2(25),
+StreetAddress,
+ZipCode,
+City,
+State,
+Primary key(ID),
+Foreign key(StreetAddress) references Address(StreetAddress),
+Foreign key(ZipCode) references Address(ZipCode),
+Foreign key(City) references Address(City),
+Foreign key(State) references Address(State));
 
 create table CustomerInteractions(
 ProNumber not null,
@@ -44,18 +79,22 @@ Foreign key(ProNumber) references Shipment(ProNumber),
 Foreign key(Shipper) references Customer(CustomerID),
 Foreign key(Consignee) references Customer(CustomerID));
 
-create table Customer(
-ID number(7) not null,
-Name varchar2(25),
-StreetAddress,
-ZipCode,
-City,
-State,
-Primary key(ID),
-Foreign key(StreetAddress) references Address(StreetAddress),
-Foreign key(ZipCode) references Address(ZipCode),
-Foreign key(City) references Address(City),
-Foreign key(State) references Address(State));
+create table Door(
+TerminalID not null,
+DoorNumber number(4) not null,
+Occupied boolean,
+Primary key(TerminalID),
+Primary key(DoorNumber),
+Foreign key(TerminalID) references Terminal(TerminalID));
+
+create table Trailer(
+TrailerNumber not null,
+DoorNumber,
+TerminalID,
+Primary key (TrailerNumber),
+Foreign key (TrailerNumber) references Entity(EntityID),
+Foreign key (DoorNumber) references Door(DoorNumber),
+Foreign key (TerminalID) references Terminal(TerminalID));
 
 create table LineHaulTravel(
 TrailerNumber not null,
@@ -69,27 +108,12 @@ Foreign key(TrailerNumber) references Trailer(TrailerNumber),
 Foreign key(Origin) references Terminal(TerminalID),
 Foreign key(Destination) references Terminal(TerminalID));
 
-create table Shipment(
-ProNumber number(8) not null,
-DueDate date,
-TotalWeight number(8),
-Price number(8),
-Primary key (ProNumber));
-
-create table ShipmentCondition(
-ProNumber not null,
-Damaged number(4),
-Short number(4),
-over number(4),
-Primary key(ProNumber));
-
-create table Door(
-TerminalID not null,
-DoorNumber number(4) not null,
-Occupied boolean,
-Primary key(TerminalID),
-Primary key(DoorNumber),
-Foreign key(TerminalID) references Terminal(TerminalID));
+create table CityHaulRoute(
+TrailerNumber not null,
+Route,
+Primary key(Trailernumber),
+Foreign key(Trailernumber) references Trailer(TrailerNumber),
+Foreign key(Route) references Entity(EntityID));
 
 create table EmployeeWorkPlace(
 EmpID not null,
@@ -97,23 +121,6 @@ TerminalID,
 Primary key(EmpID),
 Foreign key(EmpID) references Employee(EmpID),
 Foreign key(TerminalID) references Terminal(TerminalID));
-
-create table Terminal(
-TerminalID not null,
-StreetAddress,
-ZipCode,
-City,
-State,
-Primary key(TerminalID),
-Foreign key(TerminalID) references Entity(Ent),
-Foreign key(StreetAddress) references Address(StreetAddress),
-Foreign key(ZipCode) references Address(ZipCode),
-Foreign key(City) references Address(City));
-
-create table Entity(
-EntityID number(10) not null,
-Type varchar2(25),
-Primary key(EntityID));
 
 create table FloorContents(
 ProNumber not null,
@@ -138,7 +145,7 @@ EventID,
 Origin,
 Destination,
 Primary key(EventID),
-Foreign key(EventID) references Event(EventID),
+Foreign key(EventID) references LoadEvent(EventID),
 Foreign key(Origin) references Entity(EntityID),
 Foreign key(Destination) references Entity(EntityID));
 
