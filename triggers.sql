a8360c3b4e40250a00704ac4817a12bb88fd6985
Brandon Harris <harrisbm3@users.noreply.github.com>
2017-04-21 14:49:32 -0400

    Update triggers.sql
    added trigger update employeeworkplace

--- triggers.sql
+++ triggers.sql
@@ -11,4 +11,12 @@ set currentLocation = (select destination from LoadEvent where eventID=:new.even
 where proNumber=(select proNumber from LoadEvent where eventID=:new.eventID);
 end;
 
+/* We need to delete the Employee from Employee Workplace if we delete the Employee */
+create or replace trigger updateEmployeeWorkPlace
+After delete on employee
+for each row
+begin 
+delete EmployeeWorkPlace where empID=:old.empID
+end;
+
 /*Bonus trigger: if employees do more than 40 events in a day give them a bonus*/
