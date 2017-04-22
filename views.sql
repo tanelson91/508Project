b1c18866ea2a23728a4c0cc99cec98155612b74b
Brandon Harris <harrisbm3@users.noreply.github.com>
2017-04-22 15:01:06 -0400

    Update views.sql
    Added the shipmen_VU

--- views.sql
+++ views.sql
@@ -10,3 +10,10 @@ create view CustomerAddresses(Terminal_id,StreetAddress,City,ZipCode,state)
 as
 select customerID,Address.StreetAddress,Address.City,Address.ZipCode,Address.State
 from Customer join Address on customerID.AddressID=Address.AddressID;
+
+/*View for showing shipments and how many pallets and loose it has */
+create view Shipment_VU(ProNumber, DueDate, TotalWeight,price Num_Pallets, Num_Loose)
+as
+select proNumber, DueDate, TotalWeight,price Count(pallets), quantity
+from shipment natural join pallets, natural join Loose
+group by proNumber;
