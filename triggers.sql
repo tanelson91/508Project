ef2712f219128c248984b5eac0412e061672906c
Brandon Harris <harrisbm3@users.noreply.github.com>
2017-04-22 16:01:36 -0400

    Update triggers.sql
    ended up not needing to update employee work place

--- triggers.sql
+++ triggers.sql
@@ -11,12 +11,4 @@ set currentLocation = (select destination from LoadEvent where eventID=:new.even
 where proNumber=(select proNumber from LoadEvent where eventID=:new.eventID);
 end;
 
-/* We need to delete the Employee from Employee Workplace if we delete the Employee */
-create or replace trigger updateEmployeeStatus
-instead of delete on employee
-for each row
-begin 
-update employee set status='Fired' where empID=:old.empID
-end;
-
 /*Bonus trigger: if employees do more than 40 events in a day give them a bonus*/
