bfafb1caa2e8ae31063703a68b508e96286e9347
tanelson91 <nelsonta5@vcu.edu>
2017-04-23 22:33:14 -0400

    Update views.sql

--- views.sql
+++ views.sql
@@ -14,8 +14,8 @@ from Customer join Address on customerID.AddressID=Address.AddressID;
 /*View for showing shipments and how many pallets and loose it has */
 create view Shipment_VU(ProNumber, DueDate, TotalWeight,price Num_Pallets, Num_Loose)
 as
-select proNumber, DueDate, TotalWeight,price Count(pallets), quantity
-from shipment natural join pallets, natural join Loose
+select proNumber, DueDate, TotalWeight, price, Count(pallets), quantity
+from shipment natural join shipmentPallets, natural join shipmentLoose
 group by proNumber;
 
 
