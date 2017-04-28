9753c6dc9b72f67412b72d89daa83560a558cd44
Brandon Harris <harrisbm3@users.noreply.github.com>
2017-04-28 13:56:13 -0400

    Update queries.sql
    added a query for shipmentLoose and shipmentPallet

--- queries.sql
+++ queries.sql
@@ -4,3 +4,13 @@ select s.Pronumber
 from ShipmentTravels s join entity e
 on s.currentLocation=e.EntityID
 where e.EntityID='$trailer_number';
+
+/*You can use this query to view all the pallets of a given shipment*/
+select palletID, weight, cube
+from ShipmentPallets sp
+where sp.proNumber= '$proNumber';
+
+/*You can use this query to view all the pallets of a given shipment*/
+select cube, quantity
+from ShipmentLoose sl
+where sl.proNumber= '$proNumber';
